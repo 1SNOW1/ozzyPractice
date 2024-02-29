de251f531ed8e0e1c25aa5aac1cc6c911ae02a1b
enesorhan <enestcp@gmail.com>
2024-03-01 00:09:32 +0100

    initial commit

--- src/main/java/com/cydeo/Main.java
+++ src/main/java/com/cydeo/Main.java
@@ -1,2 +1,30 @@
-package com.cydeo;public class Main {
+package com.cydeo;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        ArrayList items = new ArrayList();
+        // now we don't want to add all sorts of objects into our arraylist,
+        // which is a problem because arraylist accepts every object. We need to filter our arraylist
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        items.add("Apple");
+
+
+        printDoubled(items);
+
+    }
+
+
+    private static void printDoubled(ArrayList items){
+        for (Object i : items) {
+            System.out.println((Integer)i*2);
+        }
+    }
 }
